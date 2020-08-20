7856b2a66c08067d19a14a8156ea380410112c2c
Matheusmls <69944094+Matheusmls@users.noreply.github.com>
2020-08-19 20:46:07 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/****************************
+* Hello.java
+*****************************
+* Matheus Soares Lameira
+* 08/19/2020
+* CSMC 255 003
+****************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Matheus Soares Lameira>");
+	System.out.println("<Hello.java>");
+	System.out.println("<Introduction to Java>");
+	System.out.println("<08/19/20>");
+	System.out.println("<CSMC 255 003>");
+	}
+}
+
+	
